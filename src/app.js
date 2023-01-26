da6425fa6a97fc22ae88e185ee913881b2fdf500
mindcrazy <123137817+mindcrazy@users.noreply.github.com>
2023-01-26 22:51:51 +0000

    add

---
+++ src/app.js
@@ -0,0 +1,15 @@
+//Require module
+const express = require('express');
+
+// Express Initialize
+const app = express();
+const port = 8000;
+
+//create api
+app.get('/hello_world', (req,res)=>{
+    res.send('Hello World');
+    })
+    
+app.listen(port,()=> {
+console.log('listen port 8000');
+})
